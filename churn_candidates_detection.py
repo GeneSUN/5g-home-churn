59cbe3b9a9bf1b083a0463e17fdf77802d97ec7d
Zhe Sun <injure21@gmail.com>
2024-10-08 11:51:20 -0400

    Update churn_candidates_detection.py

--- churn_candidates_detection.py
+++ churn_candidates_detection.py
@@ -22,22 +22,27 @@ from functools import reduce
 from datetime import datetime, timedelta
 
 class CPEChurnAnalyzer:
-    def __init__(self, cpe_path: str, churn_path: str):
+    def __init__(self, cpe_path: str, churn_path: str, start_date_str: str, end_date_str: str):
 
         self.cpe_path = cpe_path
         self.churn_path = churn_path
+        self.start_date_str = start_date_str
+        self.end_date_str = end_date_str
+        self.cpe_all_records = None
         self.latest_score_dates_df = None
         self.df_churn_edw = None
-        self.cpe_all_records = None
-        self.cpe_churn_candidates = None
-        self.active_candidates = None
-
-    def read_and_union_parquet_by_date(self, start_date_str: str, end_date_str: str, features=None):
+        self.df_recent_active = None
+        self.load_data()
+        self.cpe_churn_candidates = self.get_churn_customer()
+        self.active_candidates = self.get_active_customer()
+        self.cpe_churn_candidates_features = self.add_features_churn()
+        self.active_candidates_features = self.add_features_active()
+    def read_and_union_parquet_by_date(self, cpe_start_date_str: str, cpe_end_date_str: str, features=None):
         if features is None:
             features = ["dataScore","networkSpeedScore","networkSignalScore","networkFailureScore","deviceScore"]
 
-        start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
-        end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+        start_date = datetime.strptime(cpe_start_date_str, '%Y-%m-%d')
+        end_date = datetime.strptime(cpe_end_date_str, '%Y-%m-%d')
 
         dfs = []
         current_date = start_date
@@ -82,9 +87,9 @@ class CPEChurnAnalyzer:
 
         return final_df
 
-    def load_data(self, start_date_str = "2024-04-21", end_date_str =  "2024-08-29"):
-
-        self.latest_score_dates_df = self.read_and_union_parquet_by_date(start_date_str, end_date_str)\
+    def load_data(self, cpe_start_date_str = "2024-04-21", cpe_end_date_str =  "2024-08-29"):
+        self.cpe_all_records = self.read_and_union_parquet_by_date(cpe_start_date_str, cpe_end_date_str)
+        self.latest_score_dates_df = self.cpe_all_records\
                                             .withColumn("date", F.to_date(F.col("date"), "yyyy-MM-dd"))\
                                             .groupBy("cust_id", "mdn_5g")\
                                             .agg(F.max("date").alias("latest_score_date"))
@@ -92,10 +97,17 @@ class CPEChurnAnalyzer:
         self.df_churn_edw = spark.read.parquet(self.churn_path)\
                                     .withColumn("mdn_5g", F.trim(F.col("MTN")))
 
-        self.cpe_all_records = self.read_and_union_parquet_by_date(start_date_str, end_date_str)\
+        fiveg_customer_df = spark.read.parquet( hdfs_pd + "/user/ZheS/5g_Churn/fiveg_customer_df")\
+                            .withColumn("mdn_5g", F.trim(F.col("MTN")))\
+                            .withColumn("ACTIVITY_CD", F.trim(F.col("ACTIVITY_CD")))
+        self.df_recent_active = self.filter_recent_active_customers( fiveg_customer_df )
 
-    def analyze_churn(self, start_date_str: str, end_date_str: str, df_churn_edw = None, latest_score_dates_df = None):
-
+    def get_churn_customer(self, start_date_str = None, end_date_str = None, df_churn_edw = None, latest_score_dates_df = None):
+        
+        if start_date_str is None:
+            start_date_str = self.start_date_str
+        if end_date_str is None:
+            end_date_str = self.end_date_str
         if df_churn_edw is None:
             df_churn_edw = self.df_churn_edw
         if latest_score_dates_df is None:
@@ -108,12 +120,6 @@ class CPEChurnAnalyzer:
                         for i in range((end_date - start_date).days + 1)]
 
         matches_df_list = []
-        recent_active_list = []
-
-        fiveg_customer_df = spark.read.parquet( hdfs_pd + "/user/ZheS/5g_Churn/fiveg_customer_df")\
-                            .withColumn("mdn_5g", F.trim(F.col("MTN")))\
-                            .withColumn("ACTIVITY_CD", F.trim(F.col("ACTIVITY_CD")))
-        df_recent_active = self.filter_recent_active_customers( fiveg_customer_df )
 
         for deact_date in date_list:
             matches_with_date = df_churn_edw.select("CUST_ID", "mdn_5g", "deact_dt")\
@@ -121,15 +127,35 @@ class CPEChurnAnalyzer:
                                             .join(latest_score_dates_df.select("CUST_ID", "mdn_5g"), ["cust_id", "mdn_5g"])
 
             matches_df_list.append( matches_with_date )
-            recent_active_list.append( df_recent_active.filter(F.col("ACTIVITY_DT") == deact_date) )
 
-        if matches_df_list:
-            self.cpe_churn_candidates = reduce(DataFrame.union, matches_df_list)
-            self.active_candidates = reduce(DataFrame.union, recent_active_list)
+        cpe_churn_candidates = reduce(DataFrame.union, matches_df_list)
+        return cpe_churn_candidates
 
-        else:
-            print("No matches found.")
+    def get_active_customer(self, start_date_str = None, end_date_str = None, df_recent_active = None):
+        
+        if start_date_str is None:
+            start_date_str = self.start_date_str
+        if end_date_str is None:
+            end_date_str = self.end_date_str
+        if df_recent_active is None:
+            df_recent_active = self.df_recent_active
 
+        start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+        date_list = [(start_date + timedelta(days=i)).strftime('%Y-%m-%d')
+                        for i in range((end_date - start_date).days + 1)]
+
+        recent_active_list = []
+
+        for act_date in date_list:
+            df_sample = df_recent_active.filter(F.col("ACTIVITY_DT") == act_date)\
+                                        .withColumn("act_dt", col("ACTIVITY_DT"))\
+                                        .select("CUST_ID","mdn_5g","act_dt")
+            recent_active_list.append( df_sample )
+
+        active_candidates = reduce(DataFrame.union, recent_active_list)
+
+        return active_candidates
 
     def add_features_churn(self, cpe_churn_candidates = None, cpe_all_records = None, df_churn_edw = None):
         if cpe_churn_candidates is None:
@@ -140,9 +166,26 @@ class CPEChurnAnalyzer:
             df_churn_edw = self.df_churn_edw        
 
         # add cpe_features
-        cpe_churn_candidates.join(cpe_all_records, ["mdn_5g","cust_id"])\
-                            .filter( col("deact_dt")>=col("date"))\
-                            .join(df_churn_edw, ["mdn_5g","cust_id"] )
+        cpe_churn_candidates_features = cpe_churn_candidates.join( cpe_all_records, ["mdn_5g","cust_id"] )\
+                                                            .filter( col("deact_dt")>=col("date"))\
+                                                            .join( df_churn_edw.drop("deact_dt"), ["mdn_5g","cust_id"] )
+        # this should not be df_churn_edw, but more complicated data
+        return cpe_churn_candidates_features
+
+    def add_features_active(self, active_candidates = None, cpe_all_records = None, df_recent_active = None):
+        if active_candidates is None:
+            active_candidates = self.active_candidates
+        if cpe_all_records is None:
+            cpe_all_records = self.cpe_all_records
+        if df_recent_active is None:
+            df_recent_active = self.df_recent_active        
+
+        # add cpe_features
+        active_candidates_features = active_candidates.join(cpe_all_records, ["mdn_5g","cust_id"])\
+                                        .filter( col("act_dt")>=col("date"))\
+                                        .join(df_recent_active, ["mdn_5g","cust_id"] )
+
+        return active_candidates_features
 
 if __name__ == "__main__":
     mail_sender = MailSender()
@@ -153,15 +196,13 @@ if __name__ == "__main__":
     hdfs_pd = "hdfs://njbbvmaspd11.nss.vzwnet.com:9000/"
     hdfs_pa =  'hdfs://njbbepapa1.nss.vzwnet.com:9000'
 
-    analyzer = CPEChurnAnalyzer( cpe_path = hdfs_pd + "/user/ZheS/5g_homeScore/final_score", 
-                                 churn_path = hdfs_pd + "/user/ZheS/5g_Churn/churn_df")
-    analyzer.load_data()
-    analyzer.analyze_churn("2024-06-01", "2024-06-11")
+    analyzer = CPEChurnAnalyzer(cpe_path = hdfs_pd + "/user/ZheS/5g_homeScore/final_score", 
+                                churn_path = hdfs_pd + "/user/ZheS/5g_Churn/churn_df",
+                                start_date_str = "2024-06-01",
+                                end_date_str = "2024-09-05"
+                                 )
 
-    analyzer.cpe_churn_candidates.show()
-    analyzer.active_candidates.show()
-    
-    """
-    if cpe_churn_candidates:
-        cpe_churn_candidates.coalesce(1).write.csv("hdfs://path_to_output.csv", header=True)
-    """
+    analyzer.cpe_churn_candidates.write.mode("overwrite").parquet(hdfs_pd + f"/user/ZheS/5g_Churn/cpe_churn_candidates" ) 
+    analyzer.active_candidates.write.mode("overwrite").parquet(hdfs_pd + f"/user/ZheS/5g_Churn/active_candidates" ) 
+    analyzer.cpe_churn_candidates_features.write.mode("overwrite").parquet(hdfs_pd + f"/user/ZheS/5g_Churn/cpe_churn_candidates_features" ) 
+    analyzer.active_candidates_features.write.mode("overwrite").parquet(hdfs_pd + f"/user/ZheS/5g_Churn/active_candidates_features" ) 
